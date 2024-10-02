6c67604741babe88b0bbc9067f5a64a9587ff334
sharan kumar <sharanperla1234@gmail.com>
2024-10-02 11:58:54 +0530

    test: add index.test.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+      }
+    },
+    verbose: true
+  };
